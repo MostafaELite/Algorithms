2622d729d9b87241f8608f65f82f98a191e20708
MostafaELite <mostafaelite7@gmail.com>
2018-09-15 16:20:35 +0300

    -Adding Selection Sort

---
+++ Algorithms/Algorithms.Sorting/SelectionSort.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Sorting
+{
+    public class SelectionSort : SortingAlgorithm
+    {
+        public override int[] Sort(int[] unsortedArray)
+        {
+            // i represents also the last index of the sorted part of the array
+            for (int i = 0; i < unsortedArray.Length; i++)
+            {
+                int minIndex = i;
+                for (int j = i; j < unsortedArray.Length; j++)
+                    if (unsortedArray[minIndex] > unsortedArray[j])
+                        minIndex = j;
+                Swap(unsortedArray, i, minIndex);
+            }
+            return unsortedArray;
+        }
+    }
+}
--- Algorithms/Run/Program.cs
+++ Algorithms/Run/Program.cs
@@ -8,7 +8,7 @@ namespace Run
         static void Main(string[] args)
         {
             var numbers = new[] { 7, 2, 8, 5, 6, 4, 1 };
-            SortingAlgorithm sortingAlgorithm = new BubbleSort();
+            SortingAlgorithm sortingAlgorithm = new SelectionSort();            
             var sortedArray = sortingAlgorithm.Sort(numbers);
             foreach (var number in sortedArray)
                 Console.WriteLine(number);
